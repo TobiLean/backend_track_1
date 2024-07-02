c3775a03c9e5f4d05b6bde886b7a0253bdb277cb
TobiLean <Tobiloba.tsl@gmail.com>
2024-07-02 23:40:16 +0100

    update again 2

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const listener = (req, res) => {
       let q = url.parse(req.url, true).query;
       let name = q.visitor_name.replace('"', '').replace('"', '');
 
-      let greeting = `Hello, ${name}!, the temperature is ${temperature} degrees Celcius in ${location}`
+      let greeting = `Hello, ${name}!, the temperature is ${t} degrees Celcius in ${location}`
 
       console.log(req.connection.remoteAddress);
 
@@ -66,7 +66,8 @@ const listener = (req, res) => {
       ).then(
         (jsonResponse) => {
           temperature = jsonResponse.current.temp_c;
-          console.log(temperature);
+          let t = temperature.toString();
+          console.log(t);
           //console.log(jsonResponse.ip, location);
         }
       )
